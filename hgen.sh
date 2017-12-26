38139016dcb0852877f2e8ccd306dff0e888fbf1
emmanuel.meric-de-bellefon <meric.emmanuel@gmail.com>
2017-12-26 10:38:04 +0100

    script: removed outputname

--- hgen.sh
+++ hgen.sh
@@ -1,17 +1,9 @@
 #! /bin/sh
 
-if [ "$#" -le 0 ]; then
-  echo "Usage : ./hgen.sh <outputname> <search_dir>"
-  exit
-fi
-
-if [ -z "$2" ]; then
-  $2='.'
-fi
-
-files=$(find $2 -name "*.c" -type f)
+files=$(find $1 -name "*.c" -type f)
 
 if [ -z "$files" ]; then
+  echo "no *.c has been found"
   exit
 fi
 
@@ -19,7 +11,7 @@ fi
 for file in $files; do
 
   output=$(echo $file | sed 's/\.c$/\.h/g')
-  echo $output
+  echo "generating $output"
   basename=$(echo $output | grep -e '\/[^\/]\+$' -o | grep -e '[^\/]*' -o)
   format=$(echo $basename | sed 's/./\U&/g' | sed 's/\./_/g')
   
